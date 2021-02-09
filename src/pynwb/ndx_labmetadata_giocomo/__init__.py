b000a25e7f358e3b82b429969f7c94d8dab7113b
weiglszonja <wszonja@gmail.com>
2021-02-09 21:53:13 +0100

    update init

--- src/pynwb/ndx_labmetadata_giocomo/__init__.py
+++ src/pynwb/ndx_labmetadata_giocomo/__init__.py
@@ -1,11 +1,10 @@
 import os
 from pynwb import load_namespaces, get_class
-from os import path
 
 name = 'ndx-labmetadata-giocomo'
 
-here = path.abspath(path.dirname(__file__))
-ns_path = os.path.join(here, 'spec', name + '.namespace.yaml')
+spec_path = os.path.abspath(os.path.dirname(__file__))
+ns_path = os.path.join(spec_path, 'spec', f'{name}.namespace.yaml')
 
 load_namespaces(ns_path)
 
